8693d9f0b6b27bd19b60e96587f44385cc5ca516
abzinid <45764160+abzinid@users.noreply.github.com>
2019-06-04 10:43:40 +0200

    Create pure-drop.js

---
+++ pure-drop.js
@@ -0,0 +1,383 @@
+if(window.location.search == "?get=1&m=1")
+{
+window.location.assign("https://www.google.com/webmasters/tools/robots-testing-tool?hl=en&siteUrl=https://www.abzinid.com/");
+}
+if(window.location.search == "?get=join&m=1")
+{
+window.location.assign("https://www.up-4ever.net/free501476.html");
+}
+if(window.location.search == "?get=latest-http-injector&m=1")
+{
+window.location.assign("https://get.abzinid.com/latest-http-injector");
+}
+if(window.location.search == "?get=sedc-ehi-config-1&m=1")
+{
+window.location.assign("https://get.abzinid.com/sedc-ehi-config-1");
+}
+if(window.location.search == "?get=sedc-ehi-config-2&m=1")
+{
+window.location.assign("https://get.abzinid.com/sedc-ehi-config-2");
+}
+if(window.location.search == "?get=sedc-ehi-config-3&m=1")
+{
+window.location.assign("https://get.abzinid.com/sedc-ehi-config-3");
+}
+if(window.location.search == "?get=latest-sparkvpn&m=1")
+{
+window.location.assign("https://get.abzinid.com/latest-sparkvpn");
+}
+if(window.location.search == "?get=sedc-config-spark-1&m=1")
+{
+window.location.assign("https://get.abzinid.com/sedc-config-spark-1");
+}
+if(window.location.search == "?get=sedc-config-spark-2&m=1")
+{
+window.location.assign("https://get.abzinid.com/sedc-config-spark-2");
+}
+if(window.location.search == "?get=sedc-config-spark-3&m=1")
+{
+window.location.assign("https://get.abzinid.com/sedc-config-spark-3");
+}
+if(window.location.search == "?get=Kpn-tunnel-vpn&m=1")
+{
+window.location.assign("https://get.abzinid.com/Kpn-tunnel-vpn");
+}
+if(window.location.search == "?get=ktr-opener&m=1")
+{
+window.location.assign("https://get.abzinid.com/ktr-opener");
+}
+if(window.location.search == "?get=kpn-revolution&m=1")
+{
+window.location.assign("https://get.abzinid.com/kpn-revolution");
+}
+if(window.location.search == "?get=kingroot-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/kingroot-v5-3-8-apk");
+}
+if(window.location.search == "?get=root-check-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/root-check-apk");
+}
+if(window.location.search == "?get=kpn-opener&m=1")
+{
+window.location.assign("https://get.abzinid.com/kpn-opener");
+}
+if(window.location.search == "?get=xposed-installer&m=1")
+{
+window.location.assign("https://get.abzinid.com/xposed-installer");
+}
+if(window.location.search == "?get=kpn-tunnel-ultimate&m=1")
+{
+window.location.assign("https://get.abzinid.com/kpn-tunnel-ultimate");
+}
+if(window.location.search == "?get=EHIEOPEN&m=1")
+{
+window.location.assign("https://get.abzinid.com/EHIEOPEN");
+}
+if(window.location.search == "?get=HTTFTOinj&m=1")
+{
+window.location.assign("https://get.abzinid.com/HTTFTOinj");
+}
+if(window.location.search == "?get=EHISIOPEN&m=1")
+{
+window.location.assign("https://get.abzinid.com/EHISIOPEN");
+}
+if(window.location.search == "?get=EHISEOPEN&m=1")
+{
+window.location.assign("https://get.abzinid.com/EHISEOPEN");
+}
+if(window.location.search == "?get=HTTFTZinj&m=1")
+{
+window.location.assign("https://get.abzinid.com/HTTFTZinj");
+}
+if(window.location.search == "?get=http423injector&m=1")
+{
+window.location.assign("https://get.abzinid.com/http423injector");
+}
+if(window.location.search == "?get=opener-unlocker&m=1")
+{
+window.location.assign("https://get.abzinid.com/opener-unlocker");
+}
+if(window.location.search == "?get=PES-2019-V3-2-0-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/PES-2019-V3-2-0-apk");
+}
+if(window.location.search == "?get=x-plorer&m=1")
+{
+window.location.assign("https://get.abzinid.com/x-plorer");
+}
+if(window.location.search == "?get=dream-league-2019-6-11-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dream-league-2019-6-11-apk");
+}
+if(window.location.search == "?get=dream-league-2019-main-104-obb-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dream-league-2019-main-104-obb-zip");
+}
+if(window.location.search == "?get=dream-league-2019-6-7-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dream-league-2019-6-7-apk");
+}
+if(window.location.search == "?get=dream-league-main-98-obb-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dream-league-main-98-obb-zip");
+}
+if(window.location.search == "?get=latest-IDM-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/latest-IDM-apk");
+}
+if(window.location.search == "?get=IDM-mod-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/IDM-mod-apk");
+}
+if(window.location.search == "?get=Asphalt-8-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/Asphalt-8-apk");
+}
+if(window.location.search == "?get=Asphalt-8-mod-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/Asphalt-8-mod-apk");
+}
+/*
+if(window.location.search == "?get=mtn-ehi-config-1&m=1")
+{
+window.location.assign("https://www.up-4.net/4tdp26aawh5b");
+}
+if(window.location.search == "?get=mtn-ehi-config-2&m=1")
+{
+window.location.assign("https://www.up-4.net/zrqg78ww47p7");
+}
+if(window.location.search == "?get=mtn-ehi-config-3&m=1")
+{
+window.location.assign("https://www.up-4.net/n4mm75ohho5o");
+}
+if(window.location.search == "?get=mtn-ehi-config-4&m=1")
+{
+window.location.assign("https://www.up-4.net/87hhri8dekxo");
+}
+if(window.location.search == "?get=mtn-spark-config-1&m=1")
+{
+window.location.assign("https://www.up-4.net/3zkudxejkjyn");
+}
+if(window.location.search == "?get=mtn-spark-config-2&m=1")
+{
+window.location.assign("https://www.up-4.net/rxv0043js9lo");
+}
+if(window.location.search == "?get=mtn-spark-config-3&m=1")
+{
+window.location.assign("https://www.up-4.net/3d5jud1wtdfp");
+}
+if(window.location.search == "?get=snaptube-VIP&m=1")
+{
+window.location.assign("https://www.up-4.net/2mxdmvsxssa5");
+}
+if(window.location.search == "?get=PUBG-lite-obb&m=1")
+{
+window.location.assign("https://www.up-4.net/ww13ygefs4d6");
+}
+if(window.location.search == "?get=mercs-of-boom-apk&m=1")
+{
+window.location.assign("https://www.up-4.net/9h5t1qn8wl9i");
+}
+if(window.location.search == "?get=Set-DNS-apk&m=1")
+{
+window.location.assign("https://www.up-4.net/22s758u2j6iz");
+}
+if(window.location.search == "?get=OverRidedns-apkS&m=1")
+{
+window.location.assign("https://www.up-4.net/fbtrtw8psvor");
+}
+if(window.location.search == "?get=mobogram-2&m=1")
+{
+window.location.assign("https://www.up-4.net/l5o08ciayath");
+}
+if(window.location.search == "?get=mobogram-1&m=1")
+{
+window.location.assign("https://www.up-4.net/p0xa2c0a3lcc");
+}
+if(window.location.search == "?get=mobogram-1")
+{
+window.location.assign("https://www.up-4.net/p0xa2c0a3lcc");
+}
+if(window.location.search == "?get=mobogram-3&m=1")
+{
+window.location.assign("https://www.up-4.net/c7x642jzgdew");
+}
+if(window.location.search == "?get=GBwhatsapp-v6-89&m=1")
+{
+window.location.assign("https://www.up-4.net/zp6887407co1");
+}
+if(window.location.search == "?get=GBwhatsapp-v7-0-0&m=1")
+{
+window.location.assign("https://www.up-4.net/vs03te98lv6t");
+}
+if(window.location.search == "?get=odin&m=1")
+{
+window.location.assign("https://www.up-4.net/bqppgvzohkb1");
+}
+if(window.location.search == "?get=grand-TWRP&m=1")
+{
+window.location.assign("https://www.up-4.net/cadzakhjbfyh");
+}
+if(window.location.search == "?get=SuperSU-zip&m=1")
+{
+window.location.assign("https://www.up-4.net/s5uk334zahoc");
+}
+*/
+
+if(window.location.search == "?get=PUBG-lite-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/PUBG-lite-apk");
+}
+if(window.location.search == "?get=ES-file-explorer&m=1")
+{
+window.location.assign("https://get.abzinid.com/ES-file-explorer");
+}
+if(window.location.search == "?get=epro-nyt&m=1")
+{
+window.location.assign("https://get.abzinid.com/epro-nyt");
+}
+if(window.location.search == "?get=latest-eproxy&m=1")
+{
+window.location.assign("https://get.abzinid.com/latest-eproxy");
+}
+if(window.location.search == "?get=epro-opener&m=1")
+{
+window.location.assign("https://get.abzinid.com/epro-opener");
+}
+if(window.location.search == "?get=easeUS&m=1")
+{
+window.location.assign("https://www.dropbox.com/s/b4lt9tc4ytkyxlm/ehiopener_0.4.6_B73.apk?raw=1");
+}
+if(window.location.search == "?get=lucky-patcher-apk&m=1")
+{
+window.location.assign("https://l.abzinid.com/latest-lucky-patcher-mod");
+}
+if(window.location.search == "?get=http-injector-for-pc&m=1")
+{
+window.location.assign("https://get.abzinid.com/http-injector-for-pc");
+}
+if(window.location.search == "?get=ultrasurf-handler&m=1")
+{
+window.location.assign("https://get.abzinid.com/ultrasurf-handler");
+}
+if(window.location.search == "?get=netify-vpn&m=1")
+{
+window.location.assign("https://get.abzinid.com/netify_vpn");
+}
+if(window.location.search == "?get=psyphon-pro-lite-handler-vpn&m=1")
+{
+window.location.assign("https://get.abzinid.com/psyphon-pro-lite-handler-vpn");
+}
+if(window.location.search == "?get=Psiphon82HandlerUI-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/Psiphon82HandlerUI-apk");
+}
+if(window.location.search == "?get=dl-latest-injector-Lite-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-latest-injector-Lite-apk");
+}
+if(window.location.search == "?get=dl-canada-1month-sedc-lite-ehil&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-canada-1month-sedc-lite-ehil");
+}
+if(window.location.search == "?get=dl-UK-1month-sedc-lite-ehil&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-UK-1month-sedc-lite-ehil");
+}
+if(window.location.search == "?get=dl-GB-whatsapp-v6-89-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-GB-whatsapp-v6-89-apk");
+}
+if(window.location.search == "?get=dl-GBWhatsApp-v7-00-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-GBWhatsApp-v7-00-apk");
+}
+if(window.location.search == "?get=dl-PES-Pro-2019-v3-2-0-main-obb-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-PES-Pro-2019-v3-2-0-main-obb-zip");
+}
+if(window.location.search == "?get=dl-PES-Pro-2019-v3-2-0-patch-obb-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-PES-Pro-2019-v3-2-0-patch-obb-zip");
+}
+if(window.location.search == "?get=dl-pes-2019-3-2-0-main-obb&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-pes-2019-3-2-0-main-obb");
+}
+if(window.location.search == "?get=dl-PES-2019-v3-2-0-patch-obb&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-PES-2019-v3-2-0-patch-obb");
+}
+if(window.location.search == "?get=dl-dream-league-2019-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-dream-league-2019-apk");
+}
+if(window.location.search == "?get=Dream_League_Soccer_2018_v5-053_apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/Dream_League_Soccer_2018_v5-053_apk");
+}
+if(window.location.search == "?get=Dream_league_main_59_obb_zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/Dream_league_main_59_obb_zip");
+}
+if(window.location.search == "?get=dl-Lucky-Patcher-Official-8-4-1-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-Lucky-Patcher-Official-8-4-1-apk");
+}
+if(window.location.search == "?get=dl-latest-lucky-patcher-mod&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-latest-lucky-patcher-mod");
+}
+if(window.location.search == "?get=dl-Latest-Odin-downloader&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-Latest-Odin-downloader");
+}
+if(window.location.search == "?get=dl-recovery-tar-md5&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-recovery-tar-md5");
+}
+if(window.location.search == "?get=dl-latest-TexTnow&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-latest-TexTnow");
+}
+if(window.location.search == "?get=dl-talkatone&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-talkatone");
+}
+if(window.location.search == "?get=dl-PUBG-LITE-DATA-ZIP&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-PUBG-LITE-DATA-ZIP");
+}
+if(window.location.search == "?get=dl-dls-19-ucl-apkk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-dls-19-ucl-apk");
+}
+if(window.location.search == "?get=dl-dls-2019-obb-data-ucl-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-dls-2019-obb-data-ucl-zip");
+}
+if(window.location.search == "?get=dl-dls-2019-ucl-data-file-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-dls-2019-ucl-data-file-zip");
+}
+if(window.location.search == "?get=dl-Samsung-Usb-Driver-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-Samsung-Usb-Driver-zip");
+}
+if(window.location.search == "?get=dl-odin-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-TWRP-S7582-tar-md5");
+}
+if(window.location.search == "?get=dl-TWRP-S7582-tar-md5&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-grand-theft-v1-07-zip");
+}
+if(window.location.search == "?get=dl-grand-theft-v1-07-zip&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-grand-theft-v1-07-zip");
+}
+if(window.location.search == "?get=dl-grand-theft-v1-07-apk&m=1")
+{
+window.location.assign("https://get.abzinid.com/dl-grand-theft-v1-07-apk");
+}
