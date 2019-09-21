8b8978523907c831aa24ef465c7963713adcc41b
abzinid <45764160+abzinid@users.noreply.github.com>
2019-09-21 15:18:57 +0200

    Update and rename documentwrite.js to documentwrites.js

--- documentwrites.js
+++ documentwrites.js
@@ -4,7 +4,7 @@ document.write('<div>sample</div>')
 }
  if(window.location.search === "?ck")
  {   
-  document.write('<div>testing my <a class="mybutton" href="/">code sample</a></div>')
+  document.write('<div>testing my <a class="myButton" href="/">code sample</a></div>')
      // some code to load
     } else {
         document.write('<div>testing</div>')
