abd9938df3554a6ddc20a622644b130f4fa91b58
abzinid <45764160+abzinid@users.noreply.github.com>
2019-09-21 16:36:45 +0200

    Update d.js

--- d.js
+++ d.js
@@ -4,7 +4,7 @@ document.write('<div>sample</div>')
 }
  if(window.location.search === "?ck")
  {   
-  document.write('<div><h2>testing my</h2> <br/> <a class="myButton" href="/">code sample</a></div>')
+  document.write('<h2>testing my</h2> <br/> <div><a class="myButton" href="/">code sample</a></div>')
      // some code to load
     } else {
         document.write('<div>testing</div>')
